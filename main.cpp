7c6fc6269b345cea5c2a5ddf11a5b9d4e4472f4e
SEAN MALONEY - STUDENT <g00420811@atu.ie>
2026-03-09 16:16:49 +0000

    Fixing bugs

--- main.cpp
+++ main.cpp
@@ -29,55 +29,6 @@ int main() {
 		if (cmd.empty()) continue;
 		if (cmd == "QUIT") break;
 
-		if (cmd == "START") {
-			string coord; iss >> coord;
-			if (!grid.setStart(coord))
-				cout << "START failed (invalid coord or blocked).\n";
-			grid.print(false);
-			continue;
-		}
-
-		if (cmd == "GOAL") {
-			string coord; iss >> coord;
-			if (!grid.addGoal(coord))
-				cout << "GOAL failed (invalid coord or blocked).\n";
-			grid.print(false);
-			continue;
-		}
-
-		if (cmd == "OBST") {
-			string coord; iss >> coord;
-			if (!grid.toggleObstacle(coord))
-				cout << "OBST failed (invalid coord or cell is START).\n";
-			grid.print(false);
-			continue;
-		}
-
-		if (cmd == "CLEAR") {
-			string coord; iss >> coord;
-			if (!grid.clearCell(coord))
-				cout << "CLEAR failed (invalid coord).\n";
-			grid.print(false);
-			continue;
-		}
-
-		if (cmd == "CLEARALL") {
-			grid.clearAll();
-			grid.print(false);
-			continue;
-		}
-
-		if (cmd == "RUN") {
-			grid.clearPath();
-			auto path = runAStar(grid);
-			if (path.empty())
-				cout << "No path found.\n";
-			else
-				grid.markPath(path);
-			grid.print(false);
-			continue;
-		}
-
 		cout << "Unknown command.\n";
 	}
 
